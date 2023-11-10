035276d3890775dd88d92d3a16cb993990507a45
gabrielbigliardi <gabrielbigliardi@gmail.com>
2023-11-10 19:22:02 -0300

    custom handlers

--- main.go
+++ main.go
@@ -10,8 +10,12 @@ func main() {
 	fmt.Println("Hello World!")
 
 	const port = "8080"
+	const filepathRoot = "."
 
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.HandleFunc("/healthz", handlerReadiness)
+
 	corsMux := middlewareCors(mux)
 
 	srv := &http.Server{
@@ -19,19 +23,12 @@ func main() {
 		Handler: corsMux,
 	}
 
-	log.Printf("Serving on port: %s", port)
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
 }
 
-func middlewareCors(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "*")
-		if r.Method == "OPTIONS" {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
